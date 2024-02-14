eda3bc02d1f131576a8de7dc292bc9b60593f30a
Hiromasa Ono <hono@dbcls.rois.ac.jp>
2024-02-14 10:28:53 +0900

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -10,42 +10,8 @@ brew tap homebrew/science # OpenCV
 brew tap caskroom/fonts # fonts
 brew tap saulpw/vd #visidata
 
-
-# cask install
-brew install --cask google-chrome
-# ※1
-brew install --cask evernote
-# brew cask install skype
-brew install --cask vlc
-brew install --cask alfred5
-brew install --cask bettertouchtool
-brew install --cask resilio-sync
-brew install --cask firefox
-brew install --cask flux
-# brew cask install parallels
-brew install --cask slack
-# brew cask install teamviewer
-# brew cask install appcleaner
-brew install --cask iterm2
-brew install --cask rstudio
-brew install --cask betterzipql
-brew install --cask qlcolorcode
-brew install --cask qlmarkdown
-# brew install --cask qlstephen
-brew install --cask quicklook-csv
-brew install --cask quicklook-json
-# brew install --cask skala-preview
-# brew cask install libreoffice
-brew install --cask OBS
-brew install --cask vivaldi
-brew install --cask microsoft-edge
-brew install --cask visual-studio-code
-brew install --cask github
-brew install --cask deepl
-zoom install --cask zoom
-brew install --cask cmd-eikana
-
 # build or install
+brew install mas
 brew install git
 brew install openssl
 brew install wget
@@ -81,6 +47,52 @@ brew install lsd
 brew install "delphinus/sfmono-square/sfmono-square"
   # open "$(brew --prefix sfmono-square)/share/fonts"
   # open fonts with Finder
+
+# mas install
+mas install 539883307 #LINE
+mas install 497799835   #Xcode       (14.2)
+mas install 409183694   #Keynote     (12.2.1)
+mas install 408981434   #iMovie      (10.3.5)
+mas install 409201541   #Pages       (12.2.1)
+mas install 409203825   #Numbers     (12.2.1)
+mas install 424389933   #Final Cut Pro
+mas install 1176895641  #Spark - メールアプリ by Readdle
+
+# cask install
+brew install --cask google-chrome
+# ※1
+brew install --cask evernote
+# brew cask install skype
+brew install --cask vlc
+brew install --cask alfred5
+brew install --cask bettertouchtool
+brew install --cask resilio-sync
+brew install --cask firefox
+brew install --cask flux
+# brew cask install parallels
+brew install --cask slack
+# brew cask install teamviewer
+# brew cask install appcleaner
+brew install --cask iterm2
+brew install --cask rstudio
+brew install --cask betterzipql
+brew install --cask qlcolorcode
+brew install --cask qlmarkdown
+# brew install --cask qlstephen
+brew install --cask quicklook-csv
+brew install --cask quicklook-json
+# brew install --cask skala-preview
+# brew cask install libreoffice
+brew install --cask OBS
+brew install --cask vivaldi
+brew install --cask microsoft-edge
+brew install --cask visual-studio-code
+brew install --cask github
+brew install --cask deepl
+zoom install --cask zoom
+brew install --cask cmd-eikana
+
+
 
 
 # 不要なファイルを削除
